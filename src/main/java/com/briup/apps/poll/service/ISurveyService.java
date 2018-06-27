17b5e096462084e55dd801aae843fcaf896553cc
PKpansen <@MicroWin10-1622>
2018-06-27 10:31:19 +0800

    课调管理

---
+++ src/main/java/com/briup/apps/poll/service/ISurveyService.java
@@ -0,0 +1,25 @@
+package com.briup.apps.poll.service;
+
+import java.util.List;
+
+import com.briup.apps.poll.bean.survey;
+import com.briup.apps.poll.bean.extend.SurveyVM;
+
+
+
+public interface ISurveyService {
+//	查询所有
+	List<survey> findall() throws Exception;
+//	通过ID查询
+	survey findById(long id) throws Exception;
+//	关键字查询
+	List<survey> query(String keywors) throws Exception;
+	
+	List<SurveyVM> findallSurveyVM() throws Exception; 
+//	插入和更新
+	void saveOrUpdate(survey survey) throws Exception;
+//	通过ID删除
+	void deleteById(long id) throws Exception;
+//	批量删除
+	void baticDelete(Long[] ids)throws Exception;
+}
---
+++ src/main/java/com/briup/apps/poll/service/impl/SurveyService.java
@@ -0,0 +1,82 @@
+package com.briup.apps.poll.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.poll.bean.survey;
+import com.briup.apps.poll.bean.surveyExample;
+import com.briup.apps.poll.bean.extend.SurveyVM;
+import com.briup.apps.poll.dao.surveyMapper;
+import com.briup.apps.poll.dao.extend.SurveyVMMapper;
+import com.briup.apps.poll.service.ISurveyService;
+
+@Service
+
+public class SurveyService implements ISurveyService{
+	@Autowired
+	private surveyMapper surveyMapper;
+	@Autowired 
+	private SurveyVMMapper surveyVMMapper;
+	@Override
+	public List<survey> findall() throws Exception {
+		// TODO Auto-generated method stub
+		surveyExample example=new surveyExample();
+		return surveyMapper.selectByExample(example);
+				
+	}
+
+
+	@Override
+	public survey findById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		return surveyMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public List<survey> query(String keywords) throws Exception {
+		// TODO Auto-generated method stub
+		surveyExample example=new surveyExample();
+//		添加一个条件，name属性中包含keywords关键字
+		example.createCriteria().andCodeLike(keywords);
+		return surveyMapper.selectByExample(example);
+	}
+
+	@Override
+	public void saveOrUpdate(survey survey) throws Exception {
+		// TODO Auto-generated method stub
+		if(survey.getId()!=null){
+//			更新
+			surveyMapper.updateByPrimaryKey(survey);
+		}else{
+//			插入
+			surveyMapper.insert(survey);
+		}
+	}
+
+	@Override
+	public void deleteById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		surveyMapper.deleteByPrimaryKey(id);
+	}
+
+	@Override
+	public void baticDelete(Long[] ids) throws Exception {
+		// TODO Auto-generated method stub
+		for(long id : ids){
+			surveyMapper.deleteByPrimaryKey(id);
+		}
+	}
+
+
+	@Override
+	public List<SurveyVM> findallSurveyVM() throws Exception {
+		// TODO Auto-generated method stub
+		return surveyVMMapper.selectAll();
+	}
+
+
+
+
+}
